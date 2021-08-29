7dec306c240dbdd216f8718b1a9b8a06d719653d
bot-lendario <89423943+bot-lendario@users.noreply.github.com>
2021-08-29 17:16:24 -0400

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,222 @@
+const help = (prefix) => {
+return`
+<══════════════════════>
+      *ᎪΝᎫϴ ᏴϴͲ🥀*
+ <══════════════════════>
+
+ ╭╮╮╱▔▔▔▔╲╭╭╮ 
+ ╰╲╲▏▂╲╱▂▕╱╱╯
+ ┈┈╲▏▇▏▕▇▕╱┈┈
+ ┈┈╱╲▔▕▍▔╱╲┈┈ 
+ ╭╱╱ ▕╋╋╋╋▏╲╲╮
+ ╰╯╯┈╲▂▂╱┈╰╰ ╯
+ 
+       ᎪΝᎫϴ ᏴϴͲ🥀
+              𝐃𝐎𝐍𝐎 :𝐋𝐄𝐎𝐙𝐈𝐍𝐇
+                 𝐂𝐑𝐈𝐀𝐃𝐎𝐑𝐎𝐑: 𝐋𝐄𝐎𝐙𝐈𝐍𝐇
+
+╭────[ INFORMAÇÕES ]────
+│
+│➻❥ ᎪΝᎫϴ ᏴϴͲ🥀
+│➻❥ 𝐃𝐎𝐍𝐎: 𝐋𝐄𝐎𝐙𝐈𝐍𝐇
+│➻❥ Wa.me/+553597685664
+│➻❥ Status: Online✅
+│➻❥ Bot Em Testes...
+│➻❥ Prefix: 「 ${prefix} 」
+│
+╰────────────────
+
+╭──────[ FIGU ]──────
+│
+│➻❥ ${prefix}fig ou ${prefix}f
+│➻❥ ${prefix}swm nome|autor
+│➻❥ ${prefix}rename nome|autor
+│➻❥ ${prefix}toimg
+│➻❥ ${prefix}togif
+│➻❥ ${prefix}sttp
+│➻❥ ${prefix}autofigu 「1/0」
+│➻❥ ${prefix}attp1
+│➻❥ ${prefix}attp2
+│➻❥ ${prefix}attp3
+│➻❥ ${prefix}attp4
+│➻❥ ${prefix}attp5
+│➻❥ ${prefix}attp6
+│
+╰────────────────
+
+╭──────[ PLAYS ]──────
+│
+│➻❥ ${prefix}play
+│Baixa musica do yt
+│➻❥ ${prefix}playvideo
+│Baixa video do yt
+│
+╰────────────────
+
+╭────[ GRUPO ADMIN ]────
+│
+│➻❥ ${prefix}antifake「1/0」
+│➻❥ ${prefix}antilink「1/0」
+│➻❥ ${prefix}linkgp
+│➻❥ ${prefix}marcar
+│➻❥ ${prefix}marcar2
+│➻❥ ${prefix}fastvid
+│➻❥ ${prefix}pinterest ff
+│➻❥ ${prefix}pinterest (nome que vc deseja)
+│➻❥ ${prefix}bugreport
+│➻❥ ${prefix}rr
+│➻❥ ${prefix}online
+│➻❥ ${prefix}contar
+│➻❥ ${prefix}reversevid
+│➻❥ ${prefix}slowvid
+│➻❥ ${prefix}info
+│➻❥ ${prefix}tagsticker
+│➻❥ ${prefix}marcar3
+│➻❥ ${prefix}add 55xxx
+│➻❥ ${prefix}remover「@」
+│➻❥ ${prefix}promover「@」
+│➻❥ ${prefix}rebaixar
+│➻❥ ${prefix}admins
+│➻❥ ${prefix}welcome「1/0」
+│➻❥ ${prefix}abrir
+│➻❥ ${prefix}fechar
+│➻❥ ${prefix}setdesc
+│➻❥ ${prefix}fotogp
+│
+╰────────────────
+
+╭───[ APENAS O DONO ]────
+│
+│➻❥ ${prefix}ban @
+│➻❥ ${prefix}unban
+│➻❥ ${prefix}aviso
+│➻❥ ${prefix}bc
+│➻❥ ${prefix}setprefix
+│➻❥ ${prefix}hidetag
+│➻❥ ${prefix}tagsticker
+│
+╰────────────────
+
+╭────[ ADICIONADOS ]────
+│
+│➻❥ ${prefix}contar
+│➻❥ ${prefix}ler
+│➻❥ ${prefix}img
+│➻❥ ${prefix}qrcode
+│➻❥ ${prefix}wa.me
+│➻❥ ${prefix}bugreport
+│➻❥ ${prefix}bateria
+│➻❥ ${prefix}info
+│➻❥ ${prefix}dono
+│
+╰────────────────
+
+╭───[ COMANDOS DE AUDIO]────
+│
+│➻❥ ${prefix}loli1
+│➻❥ ${prefix}loli2
+│➻❥ ${prefix}canta
+│➻❥ ${prefix}acorda
+│➻❥ ${prefix}oi
+│➻❥ ${prefix}bomdia
+│➻❥ ${prefix}boatarde
+│➻❥ ${prefix}boanoit
+│
+╰────────────────
+
+╭────[ INTELIGENCIA ]────
+│
+│➻❥ ${prefix}simih 1
+│➻❥ ${prefix}simih 0
+│➻❥ ${prefix}simi
+│➻❥ prefix
+│➻❥ Oi bot
+│➻❥ Bot on?
+│➻❥ Oi
+│
+╰────────────────
+
+╭────[ %CENTAGENS ]────
+│
+│➻❥ ${prefix}%lindo
+│➻❥ ${prefix}%feio
+│➻❥ ${prefix}%gostoso
+│➻❥ ${prefix}%gado
+│➻❥ ${prefix}%gay
+│➻❥ ${prefix}gay @
+│
+╰────────────────
+
+╭────[ SEM O PREFIXO]─────
+│
+│➻❥ ${prefix}Bom dia
+│➻❥ ${prefix}Boa tarde
+│➻❥ ${prefix}Boa noite
+│➻❥ ${prefix}vai tmnc
+│➻❥ ${prefix}Bot ta on
+│➻❥ ${prefix}Bot on
+│➻❥ ${prefix}Oi bot
+│➻❥ ${prefix}Bot te amo
+│
+╰────────────────
+
+╭──────[ RANKS ]──────
+│
+│➻❥ ${prefix}gostosas
+│➻❥ ${prefix}rankgay
+│➻❥ ${prefix}rankgostoso
+│➻❥ ${prefix}rankfeios
+│➻❥ ${prefix}ranklindos
+│
+╰────────────────
+
+╭──────[ JOGOS ]──────
+│
+│➻❥ ${prefix}rr
+│➻❥ ${prefix}cassino
+│➻❥ ${prefix}cassino2
+│➻❥ ${prefix}dado
+│➻❥ ${prefix}dado2
+│➻❥ ${prefix}ttthelp
+│➻❥ ${prefix}casal
+│➻❥ ${prefix}abraço
+│➻❥ ${prefix}gay
+│➻❥ ${prefix}cc
+│
+╰────────────────
+
+╭────[ MOD AUDIO ]─────
+│
+│➻❥ ${prefix}slow
+│➻❥ ${prefix}esquilo
+│➻❥ ${prefix}nightcore
+│➻❥ ${prefix}gemuk
+│➻❥ ${prefix}fast
+│➻❥ ${prefix}bass
+│➻❥ ${prefix}estourar
+│
+╰────────────────
+
+╭────[ MOD VIDEO ]─────
+│
+│➻❥ ${prefix}slowvid
+│➻❥ ${prefix}fastvid
+│➻❥ ${prefix}reversevid
+│
+╰────────────────
+
+╭────[ DONO ]─────
+│
+│➻❥ 𝐍𝐎𝐌𝐄: 𝐀𝐍𝐉𝐎 𝐁𝐎𝐓
+│➻❥ 𝐃𝐎𝐍𝐎: 𝐋𝐄𝐎𝐙𝐈𝐍𝐇
+│➻❥ 𝐖𝐇𝐀𝐓𝐒𝐀𝐏𝐏:
+│WA.me/553597685664
+│
+╰────────────────
+
+`
+}
+
+
+
+exports.help = help
